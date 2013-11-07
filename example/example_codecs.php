17d45f5767f4226f46b6b1707075d05112907600
Buganini Q <buganini@gmail.com>
2013-11-07 19:41:18 +0800

    cleanup example

--- example/example_codecs.php
+++ example/example_codecs.php
@@ -1,8 +1,4 @@
 <?php
-
-$c=new Bsdconv('utf-8:utf-8');
-echo $c->conv('test')."\n";
-
 var_dump(bsdconv_codec_check(BSDCONV_FROM,"_utf-8"));
 var_dump(bsdconv_codec_check(BSDCONV_INTER,"_utf-8"));
 echo "From:\n";
