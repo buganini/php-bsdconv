87e8490472a360897a509b421ecb513fda264d16
Buganini Q <buganini@gmail.com>
2014-01-29 14:04:08 +0800

    restore example/example_codecs.php, which is mis-committed in previous commit

--- example/example_codecs.php
+++ example/example_codecs.php
@@ -1,12 +1,12 @@
 <?php
-var_dump(bsdconv_codec_check(BSDCONV_FROM,"_utf-8"));
-var_dump(bsdconv_codec_check(BSDCONV_INTER,"_utf-8"));
+var_dump(bsdconv_module_check(BSDCONV_FROM,"_utf-8"));
+var_dump(bsdconv_module_check(BSDCONV_INTER,"_utf-8"));
 echo "Filter:\n";
-print_r(bsdconv_codecs_list(BSDCONV_FILTER));
+print_r(bsdconv_modules_list(BSDCONV_FILTER));
 echo "From:\n";
-print_r(bsdconv_codecs_list(BSDCONV_FROM));
+print_r(bsdconv_modules_list(BSDCONV_FROM));
 echo "Inter:\n";
-print_r(bsdconv_codecs_list(BSDCONV_INTER));
+print_r(bsdconv_modules_list(BSDCONV_INTER));
 echo "To:\n";
-print_r(bsdconv_codecs_list(BSDCONV_TO));
+print_r(bsdconv_modules_list(BSDCONV_TO));
 ?>
